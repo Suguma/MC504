7f6b68ad6595c119c7b60296ee4270d156381e16
Suguma <suguma@ubuntu.(none)>
2013-03-22 17:41:39 -0300

    suguma fofo

---
+++ Unicamp/MC504/simpleshell.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int arc, char* argv[]) {
+
+  
+}
+
+char** unstring(char* argv[]) {
+  char** mander;
+  int counter = 1;
+  int i = 0;
+  int j = 0;
+  
+  while(*(argv[i]) != '\0') {
+    if(
+  
+}

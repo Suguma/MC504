edd52fc0c158fbb854589b1b109ec70d6e94fb42
Pedro <pedro@pedro-Vostro-3500.(none)>
2013-03-23 19:09:35 -0300

    moar hats

--- Unicamp/MC504/simpleshell.c
+++ Unicamp/MC504/simpleshell.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#define max 256
 
-char** unstring(char* argv[], int*,int);
+char** unstring(char*, int*,int, char*);
+void executar(char**, char**, int, int);
 
 int main(int arc, char* argv[]) {
-  int dirCounter = 0;
-  char** mander = unstring(argv,&dirCounter,0);
+  char* line;
+  int dirCounter = 0, cmdCounter = 0;
+  char** mander = unstring(argv[1], &dirCounter, 0, ":");
+  char** cmdLine;
   int i = 0;
-  int j = 0;
   
+  //gets(line);//command line
+  line = " whoami --version";
+  cmdLine = unstring(line, &cmdCounter, 1, " ");
+  executar(cmdLine, mander, dirCounter, cmdCounter);
   for(i = 0; i < 2; i++) {
     printf("%s\n", mander[i]);
   }
@@ -19,41 +26,59 @@ int main(int arc, char* argv[]) {
 }
 
 //Returns a string vector with all directors
-char** unstring(char* argv[], int* dirCounter, int extraLine) {
+char** unstring(char* str, int* dirCounter, int extraLine, char delim[]) {
   char** mander;
   char* result;
-  char delim[] = ":";
+  //  char delim[] = ":";
   int i = 0;
 
-  if(argv[1] == NULL)
+  if(str == NULL)
     return NULL;
 
   //Counts number of directories in string
   *dirCounter = 1;
-  for(i = 0; i < strlen(argv[1]); i++) {
-    if(argv[1][i] == ':')
+  for(i = 0; i < strlen(str); i++) {
+    if(str[i] == *delim)
       (*dirCounter)++;
   }
 
   //Adds an extra line to the matrix
   if(extraLine) {
     mander = malloc(sizeof(char)*(*dirCounter)+1);
-    mander[*dirCounter + 1] = NULL;
+    mander[*dirCounter] = NULL;
   }
   else
     mander = malloc(sizeof(char)*(*dirCounter));
     
   
   //Malloc for each string of the vector
-  result = strtok(argv[1],delim);
+  result = strtok(str,delim);
   i = 0;
-  while(result != NULL && i < *dirCounter) {
+  while(result != NULL && i < *dirCounter + extraLine) {
     mander[i] = malloc(sizeof(char)*(strlen(result)+1));
     strcpy(mander[i],result);
-    strcat(mander[i],"/");
+    if(!extraLine)
+      strcat(mander[i],"/");
     result = strtok(NULL,delim);
     i++;
   }
 
   return mander;
+}
+
+void executar(char** cmdLine, char** mander, int dirCounter, int cmdCounter){
+  int i = 0;
+  char* command = NULL;
+  //char* aux;//vai testar os comandos
+  printf("hastsfas\n");
+  char* aux2 = NULL;
+  strcpy(command, cmdLine[0]);//tem o comando a ser executado
+  for(i = 0; i < dirCounter; i++){
+    aux2 = malloc(sizeof(char)*max);
+    strcat(aux2, mander[i]);
+    strcat(aux2, command);
+    printf("%s", aux2);
+    free(aux2);
+  }
+  return;
 }
